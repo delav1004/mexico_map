e049e3bdeedf41fce1166cc69441292129fcbe16
Iktan <delav1004@gmail.com>
2023-03-17 17:06:19 -0600

    Added perspective and orthographic cameras.

--- Map/Camera.js
+++ Map/Camera.js
@@ -1,4 +1,5 @@
-import Map from "./Map.js"
+import * as THREE from "three";
+import Map from "./Map.js";
 
 export default class Camera {
     constructor(){
@@ -6,5 +7,31 @@ export default class Camera {
         this.sizes = this.map.sizes;
         this.scene = this.map.scene;
         this.canvas = this.map.canvas;
+        
+        this.createPerspectiveCamera();
+        this.createOrthographicCamera();
+    }
+
+    createOrthographicCamera(){
+        this.perspectiveCamera = new THREE.PerspectiveCamera(
+            35, 
+            this.sizes.aspect, 
+            0.1, 
+            1000
+        );
+        this.scene.add(this.perspectiveCamera)
+    }
+
+    createPerspectiveCamera(){
+        this.frustrum = 5;
+        this.orthographicCamera = new THREE.OrthographicCamera(
+            (-this.sizes.aspect * this.sizes.frustrum)/2,
+            (this.sizes.aspect * this.sizes.frustrum)/2,
+            this.sizes.frustrum/2,
+            -this.sizes.frustrum/2,
+            -100,
+            100
+        );
+        this.scene.add(this.orthographicCamera)
     }
 }
