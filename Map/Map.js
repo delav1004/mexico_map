4ca0f8b0cf2ad6bb47e51ddc53f2f8071176fef0
Iktan <delav1004@gmail.com>
2023-03-17 14:29:36 -0600

    Importing three into Map.js -Adding some JavaScript

--- Map/Map.js
+++ Map/Map.js
@@ -1,6 +1,12 @@
+import * as THREE from "three";
+
 export default class Map {
+    static instance
     constructor(canvas) {
+        if(Map.instance){
+            return Map.instance
+        }
+        Map.instance = this;
         this.canvas = canvas;
-        console.log("hola");
     }
 }
