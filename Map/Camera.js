4a1afcdc88f463b261a328bf26a9dc95349cc7d4
Iktan <delav1004@gmail.com>
2023-03-17 18:54:57 -0600

    Added resize function for perspective and orthographic camera.

--- Map/Camera.js
+++ Map/Camera.js
@@ -34,4 +34,21 @@ export default class Camera {
         );
         this.scene.add(this.orthographicCamera)
     }
+    resize(){
+        //Updating perspective camera on rezise 
+        this.perspectiveCamera.aspect = this.sizes.aspect;
+        this.perspectiveCamera.updateProjectionMatrix();
+
+        //Updating orthographic camera on rezise 
+        this.orthographicCamera.left = (-this.sizes.aspect * this.sizes.frustrum)/2;
+        this.orthographicCamera.right = (this.sizes.aspect * this.sizes.frustrum)/2;
+        this.orthographicCamera.top = this.sizes.frustrum/2;
+        this.orthographicCamera.bottom = -this.sizes.frustrum/2;
+
+        this.orthographicCamera.updateProjectionMatrix();
+    }
+
+    update(){
+        
+    }
 }
