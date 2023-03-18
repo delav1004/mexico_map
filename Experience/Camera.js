245e21e55f46a7af69c976b9f63d17eb4b7e17b4
Iktan <delav1004@gmail.com>
2023-03-18 01:57:29 -0600

    Added and enabled orbit controls to move around.

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import Experience from "./Experience.js";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 
 export default class Camera {
     constructor(){
@@ -10,6 +11,7 @@ export default class Camera {
         
         this.createPerspectiveCamera();
         this.createOrthographicCamera();
+        this.setOrbitControls();
     }
 
     createOrthographicCamera(){
@@ -35,6 +37,13 @@ export default class Camera {
         );
         this.scene.add(this.orthographicCamera)
     }
+
+    setOrbitControls() {
+        this.controls = new OrbitControls(this.perspectiveCamera, this.canvas)
+        this.controls.enableDamping = true;
+        this.controls.enableZoom= true;
+    }
+
     resize(){
         //Updating perspective camera on rezise 
         this.perspectiveCamera.aspect = this.sizes.aspect;
@@ -50,6 +59,6 @@ export default class Camera {
     }
 
     update(){
-
+        this.controls.update();
     }
 }
