6343a5144a713acb85c0c6c51a280c882f6b9a82
Iktan <delav1004@gmail.com>
2023-03-19 12:31:57 -0600

    Changing floor color to light blue

--- Experience/World/Floor.js
+++ Experience/World/Floor.js
@@ -13,7 +13,7 @@ export default class Floor {
     setFloor(){
         this.geometry = new THREE.PlaneGeometry(100, 100);
         this.material = new THREE.MeshStandardMaterial({
-            color: 0xffffff,
+            color: "#5E9B9D",
             side: THREE.BackSide,
         });
         this.plane = new THREE.Mesh(this.geometry, this.material);
